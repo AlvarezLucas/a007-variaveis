3c59db90f51a16240b9f44b9367b263c9293c50f
alvarezlucas <lucaasalvarez@gmail.com>
2022-08-29 19:38:35 -0500

    Exercicios1a3

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+const nome = "Lucas Alvarez"
+let idade = "25"
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+//Requisitei que me retorna se o  tipo de variavel
+
+const nomePrompt = prompt ("Qual e o seu nome?")
+console.log (nomePrompt)
+console.log(typeof nomePrompt)
+
+const idadePrompt = prompt ("Qual e a sua idade?")
+console.log (idadePrompt)
+console.log(typeof idadePrompt)
+
+// requisitei que me retorna se o tipo de variavel
+
+console.log("Ola",(nomePrompt), "voce tem",(idadePrompt), "anos de idade!")
